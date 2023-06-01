57e6b533a653272dfcb3e9e88eecfa7d6b2da9af
aefayoub <aef.ayoub@gmail.com>
2023-06-01 08:18:55 -0700

    cmt 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
  * Return: the pointer to dest.
  */
 char *_strncpy(char *dest, char *src, int n)
