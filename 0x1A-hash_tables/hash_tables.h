9837435402420819adb219bfe34146d493f1c141
aefayoub <aef.ayoub@gmail.com>
2023-09-08 13:41:00 -0700

    cmt task 0

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,26 +1,25 @@
-#ifndef __HASH__
-#define __HASH__
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "hash_tables.h"
 
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
 /* 0-hash_table_create.c */
 hash_table_t *hash_table_create(unsigned long int size);
 
 
-#endif
+#endif /* HASH_TABLES_H */
