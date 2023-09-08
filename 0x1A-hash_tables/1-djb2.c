63fcee0d2bc46697b899201fa238fe986571775e
aefayoub <aef.ayoub@gmail.com>
2023-09-08 14:15:06 -0700

    cmt task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,21 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - function that generates a random number through
- * a string given (hash number)
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
- * @str: string
- * Return: hash number
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
